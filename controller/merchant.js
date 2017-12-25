442578f9bb276e1b02a30eb5208875c87d86f215
jiangheng <jianghengamethyst@163.com>
2017-12-25 14:28:35 +0800

    node MySQL

---
+++ controller/merchant.js
@@ -0,0 +1,84 @@
+const Koa = require('koa')
+const app = new Koa()
+const Monk = require('monk')
+const db = new Monk('localhost/order')//链接到库
+const merchant = db.get('merchant')//表
+const merchantMust = db.get('merchantMust')
+const dishesCates = db.get('dishesCates')
+const dishesList = db.get('dishesList')
+const flavorList = db.get('flavorList')
+const storeCard = db.get('storeCard')
+const merchantBasicInfo = db.get('merchantBasicInfo')
+
+module.exports = {
+	// 商品信息
+	async merchantInfo(ctx) {
+		await merchant.find({merchantId:ctx.request.body.merchantId}).then(data => {
+			ctx.body = {
+		      data: data[0],
+		      code: 0,
+		      msg:'成功'
+		    }
+		})
+	},
+	//商品必选项
+	async merchantMustChoose(ctx){
+		await merchantMust.find({merchantId:ctx.request.body.merchantId}).then(data => {
+			ctx.body = {
+		      data: data[0],
+		      code: 0,
+		      msg:'成功'
+		    }
+		}) 
+	},
+	//菜品种类
+	async dishesCates(ctx){
+		await dishesCates.find({merchantId:ctx.request.body.merchantId}).then(data => {
+			ctx.body = {
+		      data: data,
+		      code: 0,
+		      msg:'成功'
+		    }
+		}) 
+	},
+	//菜品列表
+	async dishesList(ctx){
+		await dishesList.find({merchantId:ctx.request.body.merchantId}).then(data => {
+			ctx.body = {
+		      data: data,
+		      code: 0,
+		      msg:'成功'
+		    }
+		}) 
+	},
+	//口味设置
+	async flavorList(ctx){
+		await flavorList.find({merchantId:ctx.request.body.merchantId}).then(data => {
+			ctx.body = {
+		      data: data,
+		      code: 0,
+		      msg:'成功'
+		    }
+		})
+	},
+	//充值卡
+	async storeCard(ctx){
+		await storeCard.findOne({merchantId:ctx.request.body.merchantId}).then(data => {
+			ctx.body = {
+				data: data,
+				code: 0,
+				msg:'成功'
+			}
+		})
+	},
+	//商家基本信息
+	async merchantBasicInfo(ctx){
+		await merchantBasicInfo.findOne({merchantId:ctx.request.body.merchantId}).then(data => {
+			ctx.body = {
+				data: data,
+				code: 0,
+				msg:'成功'
+			}
+		})
+	}
+}
---
+++ controller/user.js
@@ -0,0 +1,96 @@
+/**
+ *个人信息 
+ */
+//连接mongodb数据库
+const Koa = require('koa')
+const app = new Koa()
+const Monk = require('monk')
+const db = new Monk('localhost/order')//链接到库
+const user = db.get('user')//表
+const checkoutParam = require('../middleware/checkoutParam')
+const fs = require('fs')
+
+module.exports = {
+	//会员注册
+  async register (ctx) {
+  	//需要校验的参数
+  	var param = {
+  		openId:'',
+  		phone:''
+  	}
+  	//参数校验
+	if(!checkoutParam(ctx,param)){
+		return new Promise(resolve => {
+			ctx.body = {
+	  			code:1,
+	  			msg:'参数错误'
+			}
+			resolve()
+		})
+	}
+  	//查询看数据库里是否有该用户
+	let tempData = null
+	await user.find({openId:ctx.request.body.openId}).then(data => {
+		tempData = data
+	})
+	if(tempData.length>0){
+		ctx.body = {
+  			code:5005,
+  			msg:'该用户已经是会员'
+		}
+	}
+	else{
+		user.insert(ctx.request.body)
+		ctx.body = {
+	      data: ctx.request.body,
+	      code: 0,
+	      msg:'成功'
+	    }
+	}
+  },
+  //用户信息查询
+  async queryById (ctx) {
+  	let tempData = null
+	await user.find({openId:ctx.request.body.openId}).then(data => {
+		tempData = data
+	})
+	//是会员
+	if(tempData.length>0){
+		ctx.body = {
+			data: tempData[0],
+			code:0,
+			msg:'查询成功'
+		}
+	}
+	else{
+		ctx.body = {
+			msg:'该用户不是会员',
+			code:1
+		}
+	}
+  },
+  //修改会员信息
+  async rewriteUserInfo (ctx) {
+  	//校验参数
+  	let tempData = null
+	await user.find({phone:ctx.request.body.phone}).then(data => {
+		tempData = data
+	})
+	//是会员
+	if(tempData.length>0){
+		await user.find({phone:ctx.request.body.phone}).then(() => {
+			user.insert(ctx.request.body)
+		})
+		ctx.body = {
+			code:0,
+			msg:'修改成功'
+		}
+	}
+	else{
+		ctx.body = {
+			msg:'该用户不是会员',
+			code:1
+		}
+	}
+  }
+}
